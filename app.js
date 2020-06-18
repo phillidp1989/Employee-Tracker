947c0ec4fa6d3a2fce6316a3db01b24c0ac6ea9b
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-18 22:12:24 +0100

    Created Add class to insert records into DB

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const { connection, View } = require("./lib/sql_queries");
+const { connection, View, Add } = require("./lib/sql_queries");
 
 const view = new View();
+const add = new Add();
 
 view.viewAllEmployees();
 view.viewAllByDepartment("Engineering");
 view.viewAllByRole("Sales Lead");
 view.viewAllByManager(5);
 view.viewAll("role");
+add.addEmployee("Dan", "Phillips", "3", "3");
