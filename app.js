f48adbb548c145a24fd77352c5c3675b27527d8a
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-06-18 21:10:17 +0100

    Created View class containing methods to retrieve data from DB

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const { connection } = require("./lib/sql_queries");
+const { connection, View } = require("./lib/sql_queries");
+
+const view = new View();
+
+view.viewAllEmployees();
+view.viewAllByDepartment("Engineering");
+view.viewAllByRole("Sales Lead");
+view.viewAllByManager(5);
+view.viewAll("role");
